968aa3d7c79b46752a13b931ce8a273e90b66a5d
Yulissa Lopez <lyulissa5@gmail.com>
2021-10-20 14:03:45 -0500

    conexion base datos

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import Express from 'express';
 import dotenv from 'dotenv';
+import {conexionBd} from './db/db.js'
 
 dotenv.config({path: './.env'});
 
 const app = Express();
 
 const main = ()=>{
-    app.listen(process.env.PORT ,()=>{
+    return app.listen(process.env.PORT ,()=>{
         console.log(`Servidor encendido ${process.env.PORT}`)
     })
 }
 
-main()
+conexionBd(main);
